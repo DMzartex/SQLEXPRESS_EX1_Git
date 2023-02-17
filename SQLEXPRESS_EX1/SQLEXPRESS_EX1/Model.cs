72a5773e917775ee7f907731ccaccc4be5d1c01a
DMzartex <91669692+DMzartex@users.noreply.github.com>
2023-02-17 14:19:24 +0100

    Ajout EX1 SQL

---
+++ SQLEXPRESS_EX1/SQLEXPRESS_EX1/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SQLEXPRESS_EX1
+{
+    class Model
+    {
+        public string CheminDB()
+        {
+            return "Data Source=PCI42107\\SQLEXPRESS;Initial Catalog=bibliotheque_bb;Integrated Security=True";
+        }
+
+
+        public bool SelectUser(out SqlDataReader resultUsers)
+        {
+
+        }
+
+
+    }
+}
